251dda8aca1bb7bab494bac56d2ce79835d8d687
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2015-12-15 00:31:29 +0000

    Flickr plugin development

--- source/_plugins/flickr.rb
+++ source/_plugins/flickr.rb
@@ -1,145 +1,136 @@
-# Flickr Photoset Tag
-#
-# A Jekyll plug-in for embedding Flickr photoset in your Liquid templates.
-#
-# Usage:
-#
-#   {% flickr_photoset 72157624158475427 %}
-#   {% flickr_photoset 72157624158475427 "Square" "Medium 640" "Large" "Site MP4" %}
-#
-# For futher information please visit: https://github.com/j0k3r/jekyll-flickr-photoset
-#
-# Author: Jeremy Benoist
-# Source: https://github.com/j0k3r/jekyll-flickr-photoset
-
-require 'flickraw'
-require 'shellwords'
-
-# FlickRaw.api_key="... Your API key ..."
-# FlickRaw.shared_secret="... Your shared secret ..."
-#
-# token = flickr.get_request_token
-# auth_url = flickr.get_authorize_url(token['oauth_token'], :perms => 'delete')
-#
-# puts "Open this url in your process to complete the authication process : #{auth_url}"
-# puts "Copy here the number given when you complete the process."
-# verify = gets.strip
-#
-# begin
-#   flickr.get_access_token(token['oauth_token'], token['oauth_token_secret'], verify)
-#   login = flickr.test.login
-#   puts "You are now authenticated as #{login.username} with token #{flickr.access_token} and secret #{flickr.access_secret}"
-# rescue FlickRaw::FailedResponse => e
-#   puts "Authentication failed : #{e.msg}"
-# end
-
 module Jekyll
+  module Flickr
+    class Flickr < Jekyll::Generator
 
-  class FlickrPhotosetTag < Liquid::Tag
-    include Jekyll::LiquidExtensions
+      DEFAULTS = {
+        'datafile' => 'photosets'
+      }
 
-    def initialize(tag_name, markup, tokens)
-      super
-      params    = Shellwords.split(markup)
-      @photoset = params[0]
-    end
-
-    def render(context)
-      # Convert a variable (for example `page.gallery_id`) into Flickr set ID
-      if @photoset =~ /([\w]+\.[\w]+)/i
-        @photoset = lookup_variable(context, @photoset)
+      def initialize(config = nil)
+        if config['jekyll-flickr'].nil?
+          @config = DEFAULTS
+        else
+          @config = Utils.deep_merge_hashes(DEFAULTS, config['jekyll-flickr'])
+        end
       end
 
-      flickrConfig = context.registers[:site].config["flickr"]
 
-      if cache_dir = flickrConfig['cache_dir']
-        if !Dir.exist?(cache_dir)
-          Dir.mkdir(cache_dir, 0777)
+      def generate(site)
+        config = site.config['jekyll-flickr']
+        data_dir = site.config['source'] + '/_data'
+        photosets_dir = data_dir + '/photosets'
+
+        puts data_dir
+        puts photosets_dir
+
+        Jekyll::Hooks.register :documents, :pre_render do |document|
+          if document.data['photoset']
+            @photoset = document.data['photoset']
+
+            puts @photoset
+
+            # Write _data folder, if doesn’t exist
+            if !Dir.exist?(data_dir)
+              puts "Creating folder to save site data in"
+              Dir.mkdir(data_dir, 0777)
+            else
+              puts "Data folder already exists"
+            end
+
+            # Write _data/photosets folder, if doesn’t exist
+            if !Dir.exist?(photosets_dir)
+              puts "Creating folder to save photoset data in"
+              Dir.mkdir(photosets_dir, 0777)
+            else
+              puts "Photoset data folder already exists"
+            end
+
+            # Write photoset data
+            path = File.join(photosets_dir, "#{@photoset}.yml")
+
+            if File.exist?(path)
+              puts "Reading data for photoset @photoset"
+              photos = YAML::load(File.read(path))
+            else
+              puts "Writing data for photoset @photoset"
+              photos = generate_photo_data(@photoset, config)
+              File.open(path, 'w') { |f| f.print(YAML::dump(photos)) }
+            end
+          end
+        end
+      end
+
+
+      def generate_photo_data(photoset, flickrConfig)
+        returnSet = Array.new
+
+        FlickRaw.api_key       = ENV['FLICKR_API_KEY'] || flickrConfig['api_key']
+        FlickRaw.shared_secret = ENV['FLICKR_SHARED_SECRET'] || flickrConfig['shared_secret']
+        flickr.access_token    = ENV['FLICKR_ACCESS_TOKEN'] || flickrConfig['access_token']
+        flickr.access_secret   = ENV['FLICKR_ACCESS_SECRET'] || flickrConfig['access_secret']
+
+        begin
+          flickr.test.login
+        rescue Exception => e
+          raise "Unable to login, please check documentation for correctly configuring Environment Variables, or _config.yaml."
         end
 
-        path = File.join(cache_dir, "#{@photoset}.yml")
-        #if File.exist?(path)
-        # photos = YAML::load(File.read(path))
-        #else
-          photos = generate_photo_data(@photoset, flickrConfig)
-          File.open(path, 'w') { |f| f.print(YAML::dump(photos)) }
-        #end
-      else
-        photos = generate_photo_data(@photoset, flickrConfig)
-      end
-    end
+        begin
+          photos = flickr.photosets.getPhotos :photoset_id => photoset
+        rescue Exception => e
+          raise "Bad photoset: #{photoset}"
+        end
 
-    def generate_photo_data(photoset, flickrConfig)
-      returnSet = Array.new
+        photos.photo.each_index do | i |
+          id    = photos.photo[i].id
+          title = photos.photo[i].title
 
-      FlickRaw.api_key       = ENV['FLICKR_API_KEY'] || flickrConfig['api_key']
-      FlickRaw.shared_secret = ENV['FLICKR_SHARED_SECRET'] || flickrConfig['shared_secret']
-      flickr.access_token    = ENV['FLICKR_ACCESS_TOKEN'] || flickrConfig['access_token']
-      flickr.access_secret   = ENV['FLICKR_ACCESS_SECRET'] || flickrConfig['access_secret']
+          exif = flickr.photos.getExif(:photo_id => id).exif.to_a
 
-      begin
-        flickr.test.login
-      rescue Exception => e
-        raise "Unable to login, please check documentation for correctly configuring Environment Variables, or _config.yaml."
-      end
+          date_created  = exif.find { |e| e.tag == "CreateDate" }
+          model         = exif.find { |e| e.tag == "Model"}
+          f_number      = exif.find { |e| e.tag == "FNumber" }
+          exposure_time = exif.find { |e| e.tag == "ExposureTime" }
+          iso           = exif.find { |e| e.tag == "ISO" }
+          focal_length  = exif.find { |e| e.tag == "FocalLength" }
 
-      begin
-        photos = flickr.photosets.getPhotos :photoset_id => photoset
-      rescue Exception => e
-        raise "Bad photoset: #{photoset}"
-      end
+          urlThumb   = String.new
+          urlEmbeded = String.new
+          urlOpened  = String.new
+          urlVideo   = String.new
 
-      photos.photo.each_index do | i |
-        id    = photos.photo[i].id
-        title = photos.photo[i].title
+          sizes = flickr.photos.getSizes(:photo_id => id)
 
-        exif = flickr.photos.getExif(:photo_id => id).exif.to_a
+          urlThumb   = sizes.find { |s| s.label == "Large Square" }
+          urlEmbeded = sizes.find { |s| s.label == "Medium 800" }
+          urlOpened  = sizes.find { |s| s.label == "Large" }
+          urlVideo   = sizes.find { |s| s.label == "Site MP4" }
 
-        date_created  = exif.find { |e| e.tag == "CreateDate" }
-        model         = exif.find { |e| e.tag == "Model"}
-        f_number      = exif.find { |e| e.tag == "FNumber" }
-        exposure_time = exif.find { |e| e.tag == "ExposureTime" }
-        iso           = exif.find { |e| e.tag == "ISO" }
-        focal_length  = exif.find { |e| e.tag == "FocalLength" }
+          photo = {
+            'id' => id,
+            'title' => title,
+            'date_created' => date_created ? date_created.raw : '',
+            'model' => model ? model.raw : '',
+            'f_number' => f_number ? f_number.raw : '',
+            'exposure_time' => exposure_time ? exposure_time.raw : '',
+            'iso' => iso ? iso.raw : '',
+            'focal_length' => focal_length ? focal_length.raw : '',
+            'urlThumb' => urlThumb ? urlThumb.source : '',
+            'urlEmbeded' => urlEmbeded ? urlEmbeded.source : '',
+            'urlOpened' => urlOpened ? urlOpened.source : '',
+            'urlVideo' => urlVideo ? urlVideo.source : '',
+            'urlFlickr' => urlVideo ? urlVideo.url : '',
+          }
 
-        urlThumb   = String.new
-        urlEmbeded = String.new
-        urlOpened  = String.new
-        urlVideo   = String.new
+          returnSet.push photo
+        end
 
-        sizes = flickr.photos.getSizes(:photo_id => id)
+        # Sleep a little to not get in trouble for bombarding Flickr servers
+        sleep 1
 
-        urlThumb   = sizes.find { |s| s.label == "Large Square" }
-        urlEmbeded = sizes.find { |s| s.label == "Medium 800" }
-        urlOpened  = sizes.find { |s| s.label == "Large" }
-        urlVideo   = sizes.find { |s| s.label == "Site MP4" }
-
-        photo = {
-          'id' => id,
-          'title' => title,
-          'date_created' => date_created ? date_created.raw : '',
-          'model' => model ? model.raw : '',
-          'f_number' => f_number ? f_number.raw : '',
-          'exposure_time' => exposure_time ? exposure_time.raw : '',
-          'iso' => iso ? iso.raw : '',
-          'focal_length' => focal_length ? focal_length.raw : '',
-          'urlThumb' => urlThumb ? urlThumb.source : '',
-          'urlEmbeded' => urlEmbeded ? urlEmbeded.source : '',
-          'urlOpened' => urlOpened ? urlOpened.source : '',
-          'urlVideo' => urlVideo ? urlVideo.source : '',
-          'urlFlickr' => urlVideo ? urlVideo.url : '',
-        }
-
-        returnSet.push photo
+        returnSet
       end
 
-      # Sleep a little to not get in trouble for bombarding Flickr servers
-      sleep 1
-
-      returnSet
     end
   end
-
 end
-
-Liquid::Template.register_tag('flickr_photoset', Jekyll::FlickrPhotosetTag)
