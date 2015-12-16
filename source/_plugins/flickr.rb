321e57fc231557923c421cb03f5d8eb074088a79
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2015-12-16 00:33:30 +0000

    Update to-do items

--- source/_plugins/flickr.rb
+++ source/_plugins/flickr.rb
@@ -1,5 +1,6 @@
 # TODO: Fix needing to run twice: once to write data, then again to read it
 # TODO: Add means of only displaying certain images from set (using tags?)
+# TODO: Add initial auth setup for FlickRaw
 
 require 'flickraw'
 
